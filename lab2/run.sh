3ed47f99fd57ed82fbf005c4ab30372b5500c1e6
rideau <93993119+adderallbaby@users.noreply.github.com>
2023-10-11 00:01:00 +0300

    Update run.sh

--- lab2/run.sh
+++ lab2/run.sh
@@ -3,19 +3,19 @@
 while getopts mah OPTION; do
   case $OPTION in
     m)
-      echo "you have selected manual" 
+      echo "manual" 
       python3 manual.py
       ;;
     a)
-      echo "auto-testing not done yet"
+      echo "testov net"
 
 
       ;;
     h | ?)
-      echo "script usage: sh run.sh [-m] [-a]\n -m for manual\n -a for auto-testing"  >&2
+      echo "usage: sh run.sh [-m] [-a]\n -m manual\n -a auto"  >&2
       exit 1
       ;;
   esac
 done
-if [ $OPTIND -eq 1 ]; then echo "No options were passed\nscript usage: sh run.sh [-m] [-a]\n -m for manual\n -a for auto-testing"; fi
+if [ $OPTIND -eq 1 ]; then echo "No options were passed\nscript usage: sh run.sh [-m] [-a]\n -m manual\n -a auto"; fi
 
